144ffaa098eafaa4c326fac7a0143aa6eb0f9d9f
Pritigamot94 <preetigamot@gmail.com>
2026-09-23 21:48:50 -0500

    added credetial

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
         'message': 'You are doing great, little human! <3',
-        'deployed_on': 'kubernetes hi'
+        'deployed_on': 'kubernetes testing docker'
     })
 
 @app.route('/api/v1/healthz')

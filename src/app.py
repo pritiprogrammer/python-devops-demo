7ba42a44c1d4aaa7078c0d90dae6f360446202b8
Pritigamot94 <preetigamot@gmail.com>
2026-09-24 17:27:01 -0500

    add argocd installiton steps

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! <3 testin',
+        'message': 'You are doing great, little human! <3 hello',
         'deployed_on': 'kubernetes testing self hosted'
     })
 

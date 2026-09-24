8d0f932504eeb2d548d857a664db8ab0ecc80d5f
Pritigamot94 <preetigamot@gmail.com>
2026-09-24 17:33:38 -0500

    udated ci cd flow

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! <3 hello',
+        'message': 'You are doing great, little human! <3',
         'deployed_on': 'kubernetes testing self hosted'
     })
 
